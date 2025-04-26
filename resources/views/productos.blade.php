fcdf5f4a3b922aa08c5acff27538156b8e0f7595
jorge chipana <jorchistauro@gmail.com>
2025-04-26 01:37:09 -0400

    Actualizar Product

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -5,7 +5,5 @@
 @section('content')
     <h2 class="mb-4 text-primary">Gestión de Productos</h2>
     <p>Aquí irá la lista de productos</p>
-    <p>Aquí irá la lista de productos</p>
-    <p>Aquí irá la lista de productos</p>
-    <p>Aquí irá la lista de productos</p>
+
 @endsection
